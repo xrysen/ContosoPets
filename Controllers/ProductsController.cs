6ae11e380d52e8552761d5a691d3bb01ecdfc145
Sean Oyler <xrysen@gmail.com>
2021-03-08 13:19:24 -0800

    Adds, Create, Read, Update, Delete routes

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -34,5 +34,45 @@ namespace ContosoPets.Api.Controllers
       }
       return product;
     }
+
+    // Add Product
+    [HttpPost]
+    public async Task<ActionResult> Create(Product product)
+    {
+      _context.Products.Add(product);
+      await _context.SaveChangesAsync();
+      return CreatedAtAction(nameof(GetById), new { id = product.Id }, product);
+    }
+
+    // Modify Product
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(int id, Product product)
+    {
+      if (id != product.Id)
+      {
+        return BadRequest();
+      }
+
+      _context.Entry(product).State = EntityState.Modified;
+      await _context.SaveChangesAsync();
+
+      return NoContent();
+    }
+
+    // Delete Product
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete (int id)
+    {
+      var product = await _context.Products.FindAsync(id);
+      if (product == null)
+      {
+        return NotFound();
+      }
+
+      _context.Products.Remove(product);
+      await _context.SaveChangesAsync();
+
+      return NoContent();
+    }
   }
 }
